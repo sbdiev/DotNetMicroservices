b1a8fdb88014d486d184711d10fe7c61ac6e5b51
Suhrob <post.suhrob@gmail.com>
2023-11-21 03:22:45 +0500

    create Catalog.API

---
+++ src/Services/Catalog/Catalog.API/Repositories/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using Catalog.API.Entities;
+
+namespace Catalog.API.Repositories
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetProduct(string id);
+        Task CreateProduct(Product product);
+        Task<bool> UpdateProduct(Product product);
+        Task<bool> DeleteProduct(string id);
+    }
+}

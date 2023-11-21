b252cec34f82c4f14be8aa814f3c783d9b161475
Suhrob <post.suhrob@gmail.com>
2023-11-22 03:30:01 +0500

    create ordering service

---
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -0,0 +1,49 @@
+using Microsoft.OpenApi.Models;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
+namespace Ordering.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApplicationServices();
+            services.AddInfrastructureServices(Configuration);
+
+            services.AddAutoMapper(typeof(Startup));
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Ordering.API", Version = "v1" });
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Ordering.API v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
